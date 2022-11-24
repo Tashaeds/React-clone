4c5eed3cc18c28bbd8ad0a5b3c6c14be6b84895d
Natasha Edwards <tashaedwards@btinternet.com>
2022-11-24 00:22:33 +0000

    fifth

--- src/Clone.js/ClonedObjects.js
+++ src/Clone.js/ClonedObjects.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {useState} from "react";
-import ClonedObjects from './Clone/ClonedObjects'
+import ClonedObjects from './Clone/ClonedObjects';
+import id1 from './img/id1.jpg';
+import id2 from './img/id1.jpg';
+import id3 from './img/id1.jpg';
+import id4 from './img/id1.jpg';
 
 function reviewee() {
   const [review, setReview] = useState([
@@ -8,7 +12,7 @@ function reviewee() {
       id: 1,
       name: "susan smith",
       job: "web developer",
-      img:
+      img:id1,
         
       text:
         "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
@@ -17,7 +21,7 @@ function reviewee() {
       id: 2,
       name: "anna johnson",
       job: "web designer",
-      img:
+      img:id2,
         
       text:
         "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
@@ -26,7 +30,7 @@ function reviewee() {
       id: 3,
       name: "peter jones",
       job: "intern",
-      img:
+      img:id3,
        
       text:
         "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
@@ -35,7 +39,7 @@ function reviewee() {
       id: 4,
       name: "bill anderson",
       job: "the boss",
-      img:
+      img:id4,
         
       text:
         "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
